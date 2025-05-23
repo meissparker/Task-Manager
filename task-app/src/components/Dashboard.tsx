9c8aeef50d40d92a2e8f8f5b4c7f5bce24c84801
meissparker <meissparker@gmail.com>
2025-05-22 20:06:37 -0500

    sixth commit

--- task-app/src/components/Dashboard.tsx
+++ task-app/src/components/Dashboard.tsx
@@ -1,16 +1,14 @@
 import  React from 'react';
 import PageLayout from './PageLayout'
 import TodoList from './TodoList'
-import { useTaskContext } from '../context/TaskContext';
 
 
 const Dashboard: React.FC = () => {
-    const { tasks } = useTaskContext();
 
     return (
         <PageLayout>
             <h2 className="mb-3">Your Tasks</h2>
-            <TodoList tasks={tasks}/>
+            <TodoList/>
         </PageLayout>
     );
 };
